359abd3b63091ebdbb120900d7e2ef7d5e7c374c
LuisMVFuentes <39308128+LuisMVFuentes@users.noreply.github.com>
2020-06-04 19:58:49 -0500

    Update init-1.js

--- js/init-1.js
+++ js/init-1.js
@@ -4,6 +4,7 @@ window.addEventListener('load', function() {
     btnAgregarCurso.addEventListener('click', agregarCursos);
     cargarHorario();
     rellenar();
+    cargarCursosList();
     autocomplete(nombre, cursosPlan);
     autocomplete(codigo, codigosCursos);
 });
